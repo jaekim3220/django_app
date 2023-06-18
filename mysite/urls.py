34d8234e413fb35fa7cd391e3d79496cd2f20d89
jaekim3220 <jehyunkim9@gmail.com>
2023-06-18 12:38:35 +0900

    django app #3 view, render, 404, templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-
-    path('', include("polls.urls")), # 8000/polls url에서 동작
-    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
